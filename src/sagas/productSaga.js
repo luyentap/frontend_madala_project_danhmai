1599411baa3bf0c883b0402e8349c088b62818e5
gaga201702 <bamboo2017002@gmail.com>
2019-03-30 22:12:24 +0700

    change api

--- src/sagas/productSaga.js
+++ src/sagas/productSaga.js
@@ -1,13 +1,13 @@
-import { take, put, call, fork, select, takeEvery, all } from 'redux-saga/effects'
+import { all, call, fork, put } from 'redux-saga/effects';
+import { fetchAllProduct } from "../actions/ActionCreaters";
 import ProductService from "../service/ProductService";
-import { fetchAllProduct} from "../actions/ActionCreaters";
 
 export function* getAllProducts() {
   const productsBest = yield call(ProductService.getBestProducts);
   const productsNew = yield call(ProductService.getNewProducts);
   const productsHot = yield call(ProductService.getHotProducts);
-  const productsAll = {"products_new":productsNew.data,"products_hot":productsHot.data,"products_best":productsBest.data}
-  console.log(productsAll)
+  const productsAll = {"products_new":productsNew.data.objects,"products_hot":productsHot.data.objects,"products_best":productsBest.data.objects}
+  console.log("productall",productsAll)
   yield put(fetchAllProduct(productsAll))
 }
 
